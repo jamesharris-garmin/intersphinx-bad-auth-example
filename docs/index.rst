f8cc0e2e949ea9dc45ae9d08ea7f643b4328840e
Jimmy Harris <james.harris@garmin.com>
2026-03-16 12:02:58 -0500

    Add example that shows the build error
    This patch adds the actual example that demonstrates the exposed
    password in our project.

---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+Welcome to my demo documentation!
+=================================
+
+This documentation is used for demonstrating a sphinx interconnect link.
+
+It refrences :external+rtd_example:doc:`index`
+
+Contents
+--------
+
+.. toctree::
+
+   Home <self>
---
+++ noxfile.py
@@ -0,0 +1,192 @@
+"""Example build jobs for intersphinx
+
+This nox file is used to run sphinx-build to validate sphinx configurations.
+"""
+
+import shutil
+from itertools import chain, repeat
+from pathlib import Path
+
+import nox
+
+PROJECT_ROOT = Path(__file__).parent
+
+SPHINX_DIR = PROJECT_ROOT / "docs"
+"The Sphinx root dir"
+
+SPHINX_SRC_DIR = SPHINX_DIR
+"The Sphinx source directory"
+
+SPHINX_OUTPUT_DIR = SPHINX_DIR / "_build"
+"The Sphinx output directory"
+
+
+# disable automatic session running.
+nox.options.sessions = ["docs(test_case='good_password_bad_path')"]
+
+# Prefer UV for speed.
+nox.options.default_venv_backend = "uv|venv"
+
+# this VENV_DIR constant specifies the name of the dir that the `dev`
+# session will create, containing the virtualenv;
+# the `resolve()` makes it portable
+VENV_DIR = (PROJECT_ROOT / ".venv").resolve()
+
+
+def install_uv_project(
+    session: nox.Session,
+    extras=None,
+    groups=None,
+    only_groups=None,
+    no_groups=None,
+    use_pip_interface=False,
+):
+    """Export the uv requirements to something pip understands.
+
+    This patch invokes uv export to export the lockfile contents to the
+
+    :param session: the current nox session.
+    :type session: nox.Session
+    """
+
+    uv_external = True
+    if session.venv_backend != "uv":
+        session.install("uv>=0.5.1")
+        uv_external = False
+
+    uv_cmd = uv_sync_cmd = [
+        "uv",
+        "sync",
+        "--quiet",
+        "--frozen",
+        "--inexact",
+    ]
+
+    uv_export_cmd = [
+        "uv",
+        "export",
+        "--quiet",
+        "--frozen",
+    ]
+
+    if use_pip_interface:
+        uv_cmd = uv_export_cmd
+
+    def _transform_list_to_arg(flag, input):
+        """Convert a list of inputs or single string input into a list of
+
+        ['--flag', input[0], ..., '--flag', input[n]]
+
+        for repeating arguments to the commandline.
+
+        :param flag: the cli flag to add in front of every element
+        :type flag: str
+        :param input: a list of elements or single string to convert.
+        :type input: string or list
+        """
+
+        def _as_list(str_or_list=None):
+            if isinstance(str_or_list, str):
+                return [str_or_list]
+
+            return str_or_list
+
+        if isinstance(input_list := _as_list(input), list):
+            return chain.from_iterable(zip(repeat(flag), input_list))
+
+        return None
+
+    if group_arg_list := _transform_list_to_arg("--group", groups):
+        uv_cmd.extend(group_arg_list)
+
+    if no_group_arg_list := _transform_list_to_arg("--no-group", no_groups):
+        uv_cmd.extend(no_group_arg_list)
+
+    if only_group_arg_list := _transform_list_to_arg("--only-group", only_groups):
+        uv_cmd.extend(only_group_arg_list)
+
+    if extras == "all" and not only_groups:
+        uv_cmd.append("--all-extras")
+    elif extras_arg_list := _transform_list_to_arg("--extra", extras):
+        uv_cmd.extend(extras_arg_list)
+
+    session.env["UV_PROJECT_ENVIRONMENT"] = session.virtualenv.location
+
+    if session.python:
+        session.env["UV_PYTHON"] = str(session.python)
+
+    if use_pip_interface:
+        req_dir = Path(session.create_tmp())
+        req_file = req_dir / "requirements.txt"
+        uv_cmd.extend(
+            ["--directory", str(req_dir.resolve()), "--output-file", str(req_file.resolve())],
+        )
+        session.run_install(*uv_cmd)
+        session.install("-r", str(req_file.resolve()))
+        shutil.rmtree(req_dir)
+    else:
+        session.run_install(
+            *uv_sync_cmd,
+            external=uv_external,
+        )
+
+
+@nox.session
+@nox.parametrize("test_case", ["bad_password", "good_password_bad_path", "happy_path"])
+def docs(session: nox.Session, test_case):
+    "Build sphinx documentation with case"
+
+    install_uv_project(session, use_pip_interface=True)
+
+    output_dir = SPHINX_OUTPUT_DIR / test_case
+
+    html_dir = output_dir / "_html"
+    doctree_dir = output_dir / "_doctree"
+
+    session.run("docker", "compose", "up", "-d", external=True)
+
+    try:
+        session.run(
+            "sphinx-build",
+            "-E",
+            "-W",
+            "-d",
+            str(doctree_dir.relative_to(PROJECT_ROOT)),
+            "-b",
+            "html",
+            str(SPHINX_SRC_DIR.relative_to(PROJECT_ROOT)),
+            str(html_dir.relative_to(PROJECT_ROOT)),
+            env={
+                "INTERSPHINX_EXAMPLE_TYPE": test_case
+            }
+        )
+    finally:
+        session.run("docker", "compose", "down", external=True)
+
+
+@nox.session(name="doc-server")
+@nox.parametrize("test_case", ["bad_password", "good_password_bad_path", "happy_path"])
+def doc_server(session: nox.Session, test_case):
+    "launch sphinx autobuild server"
+
+    install_uv_project(session, use_pip_interface=True)
+
+    output_dir = SPHINX_OUTPUT_DIR / test_case
+
+    html_dir = output_dir / "_html"
+
+    session.run("docker", "compose", "up", "-d", external=True)
+
+    try:
+        session.run(
+            "sphinx-autobuild",
+            "-a",
+            "-E",
+            str(SPHINX_SRC_DIR.relative_to(PROJECT_ROOT)),
+            str(html_dir.relative_to(PROJECT_ROOT)),
+            env={
+                "INTERSPHINX_EXAMPLE_TYPE": test_case
+            }
+        )
+    finally:
+        session.run("docker", "compose", "down", external=True)
